d360e953df36c850871767bfbcb8fa1e493de4a6
Nattapon Nimakul <totizy@gmail.com>
2019-06-20 18:34:57 +0700

    add vuex for persistance

--- src/store.js
+++ src/store.js
@@ -5,7 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    userAddress: 'full address',
+    userAddressShort: 'short address',
+    balance: '',
+    mnemonic: ''
   },
   mutations: {
 
